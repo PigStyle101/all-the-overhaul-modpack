ad7ad1733f639e9e9e400b8ab694b2413d9fe8e8
Orangedude27 <mchie.quinn@gmail.com>
2023-11-30 06:11:21 -0800

    1.5.10
    Removed remaining restack stuff

--- all-the-overhaul-modpack/data-final-fixes.lua
+++ all-the-overhaul-modpack/data-final-fixes.lua
@@ -1,8 +1,3 @@
---need to sort some more stuff, currently working.
-
--- re-stack
-require("re_stack")
-
 -- final fixes prototypes
 if mods["RampantArsenal"] then
     require("prototypes/rampant-arsenal-final")
